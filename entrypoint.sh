8c700f283a2d1ed51f189e58f958a1157a101f7f
Dave <389788728@qq.com>
2022-04-28 20:33:25 +0800

    Update entrypoint.sh
    添加跨平台支持

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ docker_file=$INPUT_DOCKER_FILE
 if [[ "${docker_file}" = "" ]]; then
     docker_file="."
 fi
-docker build -t ${image_name} ${docker_file}
+docker build --platform ${platform} -t ${image_name} ${docker_file}
 docker tag ${image_name} ${image_url}
 docker push ${image_url}
 
